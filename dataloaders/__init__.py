af1f47baa2d6a710b8dd6f70a5c3c5c82d957d15
amir <42087527+AmirMansurian@users.noreply.github.com>
2024-05-11 13:40:43 +0330

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,5 +1,6 @@
 from dataloaders.datasets import pascal
 from dataloaders.datasets import cityscapes
+from dataloaders.datasets import coco
 from torch.utils.data import DataLoader
 
 def make_data_loader(args, **kwargs):
@@ -27,6 +28,15 @@ def make_data_loader(args, **kwargs):
         test_loader = None
         
         return train_loader, val_loader, test_loader, num_class
+
+    elif args.dataset == 'coco':
+         train_set = coco.COCOSegmentation(args, split='train')
+         val_set = coco.COCOSegmentation(args, split='val')
+         num_class = train_set.NUM_CLASSES
+         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+         test_loader = None
+         return train_loader, val_loader, test_loader, num_class
         
     else:
         raise NotImplementedError
